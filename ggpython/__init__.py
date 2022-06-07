9347f7c8206bfabac30ecb3afaaa67227c118e58
nakasimas <nascor.neco@gmail.com>
2022-06-08 02:17:58 +0900

    __init__.py fix

--- ggpython/__init__.py
+++ ggpython/__init__.py
@@ -8,3 +8,10 @@ except Exception as _:
     import tracker
     import utils
     from core import *
+
+__copyright__ = "Copyright (C) 2022 nakashimas and ggpython authors"
+__version__ = "0.0.0"
+__license__ = "MIT"
+__author__ = "nakashimas"
+__author_email__ = "nascor.neco@gmail.com"
+__url__ = "https://github.com/nakashimas/ggpython"
